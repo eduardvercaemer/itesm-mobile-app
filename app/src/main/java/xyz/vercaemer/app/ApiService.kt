e497472245268899f1fef2ec1426a4492f992af6
Eduard Vercaemer <kenny@jdiaz.com>
2022-08-16 16:59:02 -0500

    add: headers interceptor

--- app/src/main/java/xyz/vercaemer/app/ApiService.kt
+++ app/src/main/java/xyz/vercaemer/app/ApiService.kt
@@ -1,18 +1,17 @@
 package xyz.vercaemer.app
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 private const val apikey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFicHZwcGJibXJraXVjdWV2bndvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA2MDQ1ODcsImV4cCI6MTk3NjE4MDU4N30.gh3rZmC--6uX-ytmhacWM3vR_XtOtE-RcbSrMJW9Eiw"
 
 private const val url = "https://abpvppbbmrkiucuevnwo.supabase.co/"
-
-private const val headers = "apikey: $apikey"
 
 data class Token(
     val access_token: String,
@@ -34,12 +33,20 @@ data class Credentials(
 
 interface ApiService {
     @POST("auth/v1/token?grant_type=password")
-    @Headers(headers)
     suspend fun signIn(@Body body: Credentials): Response<Token>
 }
 
 fun getRetrofit(): ApiService {
+    val client = OkHttpClient.Builder()
+        .addInterceptor(Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("apikey", apikey)
+                .build()
+            chain.proceed(request)
+        }).build()
+
     return Retrofit.Builder()
+        .client(client)
         .baseUrl(url)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
